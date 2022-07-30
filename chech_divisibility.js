8a7697f8e6d347e20e3d1416ee888ad2b1fde88d
vidyadhara22 <107698579+vidyadhara22@users.noreply.github.com>
2022-07-30 18:46:16 +0530

    Add files via upload

---
+++ chech_divisibility.js
@@ -0,0 +1,10 @@
+
+const Check_divisibility = (N) => {
+    if((N%6 == 0) && (N%9 == 0)){
+      return 'divisible by both';
+    }else{
+      return 'not divisible by both';
+    }
+
+    
+};
---
+++ voting.js
@@ -0,0 +1,8 @@
+
+const isEligible = (a) => {
+    if(a<18){
+      return "Not Eligible for Voting"
+    }else{
+      return "Eligible for Voting"
+    }
+};
